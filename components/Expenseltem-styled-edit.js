43b9537bdad0ed2454fc8d2e9bf5b402784aa444
natieledpaula <natiele887@gmail.com>
2026-05-24 15:35:34 -0300

    Importando os componentes nativos para construção da Interface

---
+++ components/Expenseltem-styled-edit.js
@@ -0,0 +1,13 @@
+// Importar o React e o Hook useState para controle de estado
+import React, {useState} from "react";
+
+// Importar os componentes nativos para construção da Interface
+import {
+    View, //Container de layout
+    TextInput, //Campo de entrada de texto
+    Text, //Exibição de texto
+    TouchableOpacity, // Botão clicavel
+    FlatList, // Lista de itens
+    StyleSheet, // Estilização
+    Alert, // Alertas do sistema
+} from "react-native";
